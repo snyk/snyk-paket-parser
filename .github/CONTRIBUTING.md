fde884db4290e8b298151b33a1c02602acdae567
Or Sagie <or.sagie@gmail.com>
2019-01-31 16:30:08 +0200

    feat: initial parser implementation logic stubs
    Co-authored-by: Siobhan Meier <siobhan@justshiv.com>
    Co-authored-by: Or Sagie <or.sagie@gmail.com>

--- .github/CONTRIBUTING.md
+++ .github/CONTRIBUTING.md
@@ -1,7 +1,11 @@
 # Contributing
 
 ## Contributor Agreement
+<<<<<<< HEAD
 A pull-request will only be considered for merging into the upstream codebase after you have signed our [contributor agreement](https://github.com/snyk/snyk-paket-parser/blob/master/Contributor-Agreement.md), assigning us the rights to the contributed code and granting you a license to use it in return. If you submit a pull request, you will be prompted to review and sign the agreement with one click (we use [CLA assistant](https://cla-assistant.io/)).
+=======
+A pull-request will only be considered for merging into the upstream codebase after you have signed our [contributor agreement](https://github.com/snyk/snyk/snyk-paket-parser/master/Contributor-Agreement.md), assigning us the rights to the contributed code and granting you a license to use it in return. If you submit a pull request, you will be prompted to review and sign the agreement with one click (we use [CLA assistant](https://cla-assistant.io/)).
+>>>>>>> 7ca2282... feat: initial parser implementation logic stubs
 
 ## Commit messages
 
---
+++ lib/index.ts
@@ -0,0 +1,108 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as _ from 'lodash';
+
+export {
+  parseLockFile,
+  parseDependenciesFile,
+};
+
+/*
+input:
+NUGET
+  remote: https://nuget.org/api/v2
+    FSharp.Core (4.0.0.1)
+      Newtonsoft.Json (7.0.1)
+    UnionArgParser (0.6.3)
+
+GITHUB
+  remote: forki/FsUnit
+    FsUnit.fs (81d27fd09575a32c4ed52eadb2eeac5f365b8348)
+
+Group test
+NUGET
+  remote: https://nuget.org/api/v2
+    FSharp.Core (4.0.0.1)
+GITHUB
+  remote: forki/FsUnit
+    FsUnit.fs (81d27fd09575a32c4ed52eadb2eeac5f365b8348)
+  remote: fsharp/FAKE
+    src/app/FakeLib/Globbing/Globbing.fs
+
+ */
+function parseLockFile(path: string) {
+  if (!fs.existsSync(path)) {
+    throw new Error('No project file found at ' +
+      `location: ${path}`);
+  }
+
+  const fileContents = fs.readFileSync(path, 'utf-8');
+  let lines = fileContents.split('\n');
+
+  if (!lines[0].includes('GROUP')) {
+    lines = ['GROUP main'].concat(lines); //put in a different array
+  }
+
+  // algorithm
+  // take group, repo type, repo link
+  // call parseSpaceIndentedStructureToJSON for lines under those
+
+
+
+}
+
+/*
+input:
+NUGET
+  remote: https://nuget.org/api/v2
+    FSharp.Core (4.0.0.1)
+      Newtonsoft.Json (7.0.1)
+    UnionArgParser (0.6.3)
+output:
+{
+  "group main": {
+    "NUGET": {
+    "remote: https://nuget.org/api/v2": {
+      "FSharp.Core (4.0.0.1)": {
+        "Newtonsoft.Json (7.0.1)": {}
+      },
+      "UnionArgParser (0.6.3)": {}
+    }
+  }
+}
+
+ */
+function parseSpaceIndentedStructure (group: string[]) {
+  // we are not processing non NUGET repos
+  // if (group[0].trim() !== 'NUGET') {
+  //   return null;
+  // }
+
+}
+
+/*
+input:
+    FSharp.Core (4.0.0.1)
+      Newtonsoft.Json (7.0.1)
+    UnionArgParser (0.6.3)
+ */
+function parseGroupRepoDepenencies(rows: string[]) {
+
+}
+
+/*
+input:
+    FSharp.Core (4.0.0.1)
+ */
+function parseLockFileRow (row: string) {
+  const rowParts = row.trim().split(' - restriction: ');
+  const dependencyParts = rowParts[0].split(' ');
+
+  return {
+    name: dependencyParts[0],
+    version: dependencyParts[1].slice(1, -1),
+    restriction: rowParts[1],
+  }
+}
+
+
