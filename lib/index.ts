85bb4cefbc9e9c948abf643076da9db56f11acfb
Kirill Efimov <kirill@snyk.io>
2019-02-14 12:12:20 +0200

    fix: finishing up deptree refactoring

--- lib/index.ts
+++ lib/index.ts
@@ -1,25 +1,25 @@
-import {parseLockFile, PaketLock, Dependency} from './lock-parser';
+import {parseLockFile, PaketLock} from './lock-parser';
 import {parseDependenciesFile, PaketDependencies, NugetDependency} from './dependencies-parser';
 import * as path from 'path';
 import * as fs from 'fs';
-import {InvalidUserInputError} from './errors';
-// import * as util from 'util';
+import {InvalidUserInputError, OutOfSyncError} from './errors';
 
 const DEV_GROUPS = ['build', 'test', 'tests'];
-const FREQUENCY_THRESHOLD = 2;
+const SUPPORTED_SOURCES = ['nuget'];
+const FREQUENCY_THRESHOLD = 100;
 
-interface DepCollection { [dep: string]: {}; }
+export {InvalidUserInputError, OutOfSyncError};
 
 export interface DepTree {
   name: string;
   version: string;
-  dependencies: DepCollection;
+  dependencies: {
+    [dep: string]: DepTree;
+  };
   depType?: DepType;
   hasDevDependencies?: boolean;
-  cyclic?: boolean;
   targetFrameworks?: string[];
-  refs?: number;
-  root?: boolean;
+  missingLockFileEntry?: boolean;
 }
 
 export enum DepType {
@@ -31,8 +31,10 @@ interface FlatDependency {
   name: string;
   version: string;
   dependencies: string[];
+  depType: DepType;
   root: boolean;
   refs: number;
+  resolved: boolean;
 }
 
 export async function buildDepTreeFromFiles(
@@ -70,192 +72,159 @@ export async function buildDepTree(
   const manifestFile = parseDependenciesFile(manifestFileContents);
   const lockFile = parseLockFile(lockFileContents);
 
-  const tree = buildTreeFromParsed(manifestFile, lockFile, includeDev);
-
-  tree.name = defaultManifestFileName;
-
-  return tree;
-}
-
-function buildTreeFromParsed(
-  manifestFile: PaketDependencies,
-  lockFile: PaketLock,
-  includeDev: boolean = false,
-) {
   const depTree = {
     dependencies: {},
-    name: '',
+    name: defaultManifestFileName,
     version: '',
   } as DepTree;
 
-  let dependencies = {} as DepCollection;
-  let dependenciesMap = {} as DepCollection;
+  const dependenciesMap: Map<string, FlatDependency> = new Map();
 
-  dependencies = getRootDeps(manifestFile);
-  dependenciesMap = getResolvedDeps(lockFile, dependencies);
+  collectRootDeps(manifestFile, dependenciesMap);
+  collectResolvedDeps(lockFile, dependenciesMap);
 
-  for (const mapped in dependenciesMap) {
-    if (dependenciesMap.hasOwnProperty(mapped)) {
-      calculateReferences(dependenciesMap[mapped], dependenciesMap);
+  for (const dep of dependenciesMap.values()) {
+    if (dep.root) {
+      calculateReferences(dep, dependenciesMap);
     }
   }
 
-  const frequent = getFrequentDependencies(dependenciesMap);
-  const frequentSubTree = buildFrequentDepsSubtree(dependenciesMap);
+  for (const dep of dependenciesMap.values()) {
+    if (dep.root && (includeDev || dep.depType === DepType.prod)) {
+      if (strict && !dep.resolved) {
+        throw new OutOfSyncError(dep.name);
+      }
 
-  // console.log(util.inspect(JSON.stringify(frequent), false, null, true))
-  // console.log(util.inspect(JSON.stringify(frequentSubTree), false, null, true))
+      depTree.dependencies[dep.name] = buildTreeFromList(dep, dependenciesMap);
 
-  for (const group of lockFile.groups) {
-    const isDev = DEV_GROUPS.indexOf((group.name || '').toLowerCase()) !== -1;
-
-    if (isDev && !includeDev) {
-      continue;
-    }
-
-    for (const dep of group.dependencies) {
-      if (dependencies[dep.name]) {
-        dependencies[dep.name] = {
-          depType: isDev ? DepType.dev : DepType.prod,
-          dependencies: buildSubTree(dep.dependencies, group.dependencies, isDev),
-          name: dep.name,
-          version: dep.version,
-        };
+      if (!dep.resolved) {
+        depTree.dependencies[dep.name].missingLockFileEntry = true;
       }
     }
   }
 
-  depTree.dependencies = dependencies;
+  const frequentSubTree = buildFrequentDepsSubtree(dependenciesMap);
+
+  if (Object.keys(frequentSubTree.dependencies).length) {
+    depTree.dependencies[frequentSubTree.name] = frequentSubTree;
+  }
 
   return depTree;
 }
 
-function buildSubTree(dependencies: any, groupDeps: Dependency[], isDev: boolean) {
-  const subTree = {} as DepCollection;
-
-  for (const currDep of dependencies) {
-    for (const dep of groupDeps) {
-      if (dep.name === currDep.name) {
-        subTree[dep.name] = {
-          depType: isDev ? DepType.dev : DepType.prod,
-          dependencies: buildSubTree(dep.dependencies, groupDeps, isDev),
-          name: dep.name,
-          version: dep.version,
-        };
-      }
-    }
-  }
-  return subTree;
-}
-
-function createDependency(dependency: any, root: boolean) {
-  return {
-    name: dependency.name,
-    version: dependency.version,
-    dependencies: dependency.dependencies,
-    root,
-    refs: root ? 1 : 0,
-  };
-}
-
-function getRootDeps(manifestFile: PaketDependencies) {
-  const rootDeps = {} as DepCollection;
-
+function collectRootDeps(manifestFile: PaketDependencies, dependenciesMap: Map<string, FlatDependency>) {
   for (const group of manifestFile) {
+    const isDev = DEV_GROUPS.indexOf((group.name || '').toLowerCase()) !== -1;
+
     for (const dep of group.dependencies) {
-      const current = dep as NugetDependency;
-      if (current.name) {
-        rootDeps[current.name] = {
-          name: current.name,
-          version: null as string,
-          dependencies: null as DepCollection,
+      if (SUPPORTED_SOURCES.indexOf(dep.source.toLowerCase()) === -1) {
+        continue;
+      }
+
+      const nugetDep = dep as NugetDependency;
+
+      if (!dependenciesMap.has(nugetDep.name.toLowerCase())) {
+        dependenciesMap.set(nugetDep.name.toLowerCase(), {
+          name: nugetDep.name,
+          // Will be overwritten in `collectResolvedDeps`.
+          version: nugetDep.versionRange,
+          // Will be overwritten in `collectResolvedDeps`.
+          dependencies: [],
+          depType: isDev ? DepType.dev : DepType.prod,
           root: true,
           refs: 1,
-        } as DepTree;
+          // Will be overwritten in `collectResolvedDeps`.
+          resolved: false,
+        });
       }
     }
   }
-
-  return rootDeps;
 }
 
-function getResolvedDeps(lockFile: PaketLock, rootDeps: DepCollection) {
-  const resolvedDeps = {} as DepCollection;
+function collectResolvedDeps(lockFile: PaketLock, dependenciesMap: Map<string, FlatDependency>) {
   for (const group of lockFile.groups) {
-    const isDev = DEV_GROUPS.includes((group.name || '').toLowerCase());
+    const isDev = DEV_GROUPS.indexOf((group.name || '').toLowerCase()) !== -1;
 
     for (const dep of group.dependencies) {
-      resolvedDeps[dep.name] = {
-        name: dep.name,
-        version: dep.version,
-        dependencies: buildSubTree(dep.dependencies, group.dependencies, isDev),
-        depType: isDev ? DepType.dev : DepType.prod,
-        root: rootDeps[dep.name] ? true : false,
-        refs: rootDeps[dep.name] ? 1 : 0,
-      } as DepTree;
-    }
-  }
-  return resolvedDeps;
-}
+      if (SUPPORTED_SOURCES.indexOf(dep.repository.toLowerCase()) === -1) {
+        continue;
+      }
 
-function calculateReferences(node: any, dependencies: DepCollection) {
-  for (const subName in node.dependencies) {
-    if (node.dependencies.hasOwnProperty(subName)) {
-      const sub = dependencies[subName] as DepTree;
+      if (dependenciesMap.has(dep.name.toLowerCase())) {
+        const rootDep = dependenciesMap.get(dep.name.toLowerCase());
 
-      sub.refs += node.refs;
-
-      // Do not propagate calculations if we already reach threshold for the node.
-      if (sub.refs < FREQUENCY_THRESHOLD) {
-        calculateReferences(sub, dependencies);
+        rootDep.version = dep.version;
+        rootDep.dependencies = dep.dependencies.map((d) => d.name.toLowerCase());
+        rootDep.resolved = true;
+      } else {
+        dependenciesMap.set(dep.name.toLowerCase(), {
+          name: dep.name,
+          version: dep.version,
+          dependencies: dep.dependencies.map((d) => d.name.toLowerCase()),
+          depType: isDev ? DepType.dev : DepType.prod,
+          root: false,
+          refs: 0,
+          resolved: true,
+        });
       }
     }
   }
 }
 
-function buildFrequentDepsSubtree(dependencies: DepCollection) {
-  const tree = {
+function calculateReferences(node: FlatDependency, dependenciesMap: Map<string, FlatDependency>) {
+  for (const subName of node.dependencies) {
+    const sub = dependenciesMap.get(subName);
+
+    sub.refs += node.refs;
+
+    // Do not propagate calculations if we already reach threshold for the node.
+    if (sub.refs < FREQUENCY_THRESHOLD) {
+      calculateReferences(sub, dependenciesMap);
+    }
+  }
+}
+
+function buildFrequentDepsSubtree(dependenciesMap: Map<string, FlatDependency>): DepTree {
+  const tree: DepTree = {
     name: 'meta-common-packages',
     version: 'meta',
-    dependencies: {} as DepCollection,
+    dependencies: {},
   };
 
-  getFrequentDependencies(dependencies).forEach((listItem: DepTree) => {
-    const treeNode = buildTreeFromList(listItem, dependencies);
+  getFrequentDependencies(dependenciesMap).forEach((listItem: FlatDependency) => {
+    const treeNode = buildTreeFromList(listItem, dependenciesMap);
     tree.dependencies[treeNode.name] = treeNode;
   });
 
   return tree;
 }
 
-function getFrequentDependencies(dependencies: DepCollection) {
-  const frequentDeps = [] as any;
-  for (const depName in dependencies) {
-    if (dependencies.hasOwnProperty(depName)) {
-      const current = dependencies[depName] as DepTree;
-      if (!current.root &&  current.refs >= FREQUENCY_THRESHOLD) {
-        frequentDeps.push(current);
-      }
+function getFrequentDependencies(dependenciesMap: Map<string, FlatDependency>): FlatDependency[] {
+  const frequentDeps = [];
+
+  for (const dep of dependenciesMap.values()) {
+    if (!dep.root && dep.refs >= FREQUENCY_THRESHOLD) {
+      frequentDeps.push(dep);
     }
   }
+
   return frequentDeps;
 }
 
-function buildTreeFromList(listItem: DepTree, dependencies: DepCollection) {
+function buildTreeFromList(listItem: FlatDependency, dependenciesMap: Map<string, FlatDependency>) {
   const tree = {
     name: listItem.name,
-    version: listItem.version!,
-    dependencies: {} as DepCollection,
-  };
+    version: listItem.version,
+    dependencies: {},
+    depType: listItem.depType,
+  } as DepTree;
 
-  for (const name in listItem.dependencies) {
-    if (listItem.dependencies.hasOwnProperty(name)) {
-      const subListItem = dependencies[name] as DepTree;
+  for (const name of listItem.dependencies) {
+    const subListItem = dependenciesMap.get(name);
 
-      if (!(subListItem.refs >= FREQUENCY_THRESHOLD)) {
-        const subtree = buildTreeFromList(subListItem, dependencies);
-        tree.dependencies[subtree.name] = subtree;
-      }
+    if (!(subListItem.refs >= FREQUENCY_THRESHOLD)) {
+      const subtree = buildTreeFromList(subListItem, dependenciesMap);
+      tree.dependencies[subtree.name] = subtree;
     }
   }
 
