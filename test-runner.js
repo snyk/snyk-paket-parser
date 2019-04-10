2a5f226f3db2d545253fa89fafa5754b26a4a869
Konstantin Yegupov <konstantin@snyk.io>
2019-04-10 13:43:04 +0100

    chore: drop Node 4 support

--- test-runner.js
+++
@@ -1,9 +0,0 @@
-const cp = require('child_process');
-const v = String(cp.execSync('node -v'));
-
-if (v.startsWith('v4.')) {
-  cp.execSync('npm run build');
-  require('./test/node4');
-} else {
-  require('jest').run();
-}
